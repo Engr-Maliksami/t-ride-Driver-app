4916c87dc9344a16eb2881f36f2f8783af55133b
Mohammad Abdulsamad <163730186+MohammadAbdulSamad1@users.noreply.github.com>
2026-05-20 13:59:08 +0500

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,18 +1,7 @@
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
@@ -20,5 +9,4 @@ subprojects {
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
